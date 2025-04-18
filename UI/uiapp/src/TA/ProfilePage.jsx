a7eff35dd27fbd7ec0f6fe08889c555933f013f7
Başar Çubukçuoğlu <basarcubukcuoglu@gmail.com>
2025-04-18 19:22:26 +0300

    TA Profile backend-frontend connection finished

--- UI/uiapp/src/TA/ProfilePage.jsx
+++ UI/uiapp/src/TA/ProfilePage.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProfilePage.css";
 import { useNavigate } from "react-router-dom";
 import Navbar from "./Navbar";
+import axios from "axios";
 
 const ProfilePage = () => {
   const navigate = useNavigate();
   const [showChangePasswordModal, setShowChangePasswordModal] = React.useState(false);
   const [showUnavailabilityModal, setShowUnavailabilityModal] = React.useState(false);
+  const [taProfileInfo, setTaProfileInfo] = useState({}); // Initialize as an empty object");
+
+  useEffect(() => {
+    const fetchProfileInformation = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/ta/profile?id=2"); // Adjust the URL as needed
+        setTaProfileInfo(response.data);
+        console.log(taProfileInfo); // Log the fetched tasks to the console
+        //console.log("TasProctorings:", response.data); // Log the fetched tasks to the console
+        // You can set the tasks to state if needed
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
+    };
+    fetchProfileInformation();
+  }, []);
 
   return (
     <div className="profile-container">
@@ -15,13 +32,13 @@ const ProfilePage = () => {
       <div className="profile-content">
         <div className="info-card">
           <h3>Personal Information</h3>
-          <p><strong>Name</strong><br />Ahmet</p>
-          <p><strong>Surname</strong><br />Yılmaz</p>
-          <p><strong>Email</strong><br />ahmet.yilmaz@ug.bilkent.edu.tr</p>
-          <p><strong>ID</strong><br />34346543</p>
-          <p><strong>Role</strong><br />Teaching Assistant</p>
-          <p><strong>Department</strong><br />Computer Science</p>
-          <p><strong>Course</strong><br />CS 202</p>
+          <p><strong>Name</strong><br />{taProfileInfo.name}</p>
+          <p><strong>Surname</strong><br />{taProfileInfo.surname}</p>
+          <p><strong>Email</strong><br />{taProfileInfo.email}</p>
+          <p><strong>ID</strong><br />{taProfileInfo.bilkentId}</p>
+          <p><strong>Role</strong><br />{taProfileInfo.role}</p>
+          <p><strong>Department</strong><br />{taProfileInfo.departmentName}</p>
+          <p><strong>Course</strong><br />{taProfileInfo.courseName}</p>
         </div>
 
         <div className="right-section">
