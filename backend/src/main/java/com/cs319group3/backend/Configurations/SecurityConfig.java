ce55918f0df2f691b45628fe8a01717192e6a315
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2025-04-23 16:06:22 +0300

    endpoints are relieved

--- backend/src/main/java/com/cs319group3/backend/Configurations/SecurityConfig.java
+++ backend/src/main/java/com/cs319group3/backend/Configurations/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         //.requestMatchers("/auth/login").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
