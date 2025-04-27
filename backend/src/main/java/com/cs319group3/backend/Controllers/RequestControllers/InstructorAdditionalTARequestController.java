ac709fd1276840a03251432fa15b5a2a4a84c097
Serdar Bozdağ <serdarbozdag03@gmail.com>
2025-04-27 23:09:18 +0300

    Fetching the instructor_additional_ta_request for the dean

--- backend/src/main/java/com/cs319group3/backend/Controllers/RequestControllers/InstructorAdditionalTARequestController.java
+++ backend/src/main/java/com/cs319group3/backend/Controllers/RequestControllers/InstructorAdditionalTARequestController.java
@@ -1,16 +1,27 @@
 package com.cs319group3.backend.Controllers.RequestControllers;
 
 
+import com.cs319group3.backend.DTOs.InstructorAdditionalTARequestDTO;
+import com.cs319group3.backend.Entities.RequestEntities.InstructorAdditionalTARequest;
+import com.cs319group3.backend.Services.InstructorAdditionalTARequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("taFromDeanRequest")
 @ComponentScan(basePackages = {"com.cs319group3.backend.Controllers.RequestControllers"})
 @CrossOrigin(origins = "http://localhost:3000")
 public class InstructorAdditionalTARequestController {
-
+    @Autowired
+    InstructorAdditionalTARequestService instructorAdditionalTARequestService;
+    //Fetches all the instructor additional ta request from database using the give receiver id
+    @GetMapping("getInstructorAdditionalTARequests")
+    public List<InstructorAdditionalTARequestDTO> getInstructorAdditionalTARequests(@RequestParam int receiverId) {
+        System.out.println("Fetching Instructor Additional TA Requests for dean");
+        return instructorAdditionalTARequestService.getInstructorAdditionalTARequests(receiverId);
+    }
 
 }
