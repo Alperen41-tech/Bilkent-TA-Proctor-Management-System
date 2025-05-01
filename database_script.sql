61614391bcdf575e52b5a58d0fd8a4ca160d9c75
Ata Eren KILIÇ <ataerenkilic@gmail.com>
2025-05-02 00:28:58 +0300

    script change

--- database_script.sql
+++ database_script.sql
@@ -491,7 +491,9 @@ INSERT INTO request (request_id, sender_user_id, receiver_user_id, sent_date, is
 	(17, 1, 6, '2025-05-22 10:00:00', true, '2025-05-27 10:00:00', 'Request 17 for from_dean'),
 	(18, 1, 3, '2025-05-23 10:00:00', false, null, 'Request 18 for from_dean'),
 	(19, 7, 4, '2025-05-24 10:00:00', true, '2025-05-27 10:00:00', 'Request 19 for from_dean'),
-	(20, 7, 8, '2025-05-25 10:00:00', false, null, 'Request 20 for from_dean');
+	(20, 7, 8, '2025-05-25 10:00:00', false, null, 'Request 20 for from_dean'),
+	(28, 7, 9, '2025-05-06 8:00:00', true, '2025-05-10 10:00:00' , 'Request 1 for swap'),
+	(29, 7, 9, '2025-05-06 10:00:00', false, '2025-05-10 10:00:00' , 'Request 1 for swap');
 
 INSERT INTO ta_swap_request (request_id, class_proctoring_id) VALUES
 	(1, 2),
@@ -521,7 +523,9 @@ INSERT INTO instructor_additional_ta_request (request_id, ta_count, is_sent_to_s
 	(17, 2, true, 3),
 	(18, 2, false, 2),
 	(19, 2, true, 1),
-	(20, 1, false, 5);
+	(20, 1, false, 5),
+	(28, 2, false, 3),
+	(29, 2, false, 3);
     
 INSERT INTO class_proctoring_ta_relation (class_proctoring_id, ta_id, is_paid, is_complete, is_open_to_swap) VALUES
 	(1, 3, false, false, true),
