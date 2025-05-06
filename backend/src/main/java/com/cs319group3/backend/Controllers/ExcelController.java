468165886b41a8b8b59035cc70c24e8f915002d3
Alperen Karslıoğlu <a.karslioglu@ug.bilkent.edu.tr>
2025-05-06 15:53:52 +0300

    excel file can be composed and can be downloaded now error fixed

--- backend/src/main/java/com/cs319group3/backend/Controllers/ExcelController.java
+++ backend/src/main/java/com/cs319group3/backend/Controllers/ExcelController.java
@@ -3,12 +3,14 @@ package com.cs319group3.backend.Controllers;
 import com.cs319group3.backend.Services.ExcelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("excel")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ExcelController {
 
     @Autowired
